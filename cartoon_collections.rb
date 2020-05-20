0cfac9b1b3ecab8f22c1cc715a188877373c989e
Ted Neben <tedneben@gmail.com>
2020-05-20 21:58:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,11 @@
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
+  array.each { |num| 
+end
+
+# Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
+
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
