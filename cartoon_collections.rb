c3ebcbe5613a3a7250c090add1ff1a7ec7fc9d7c
Ted Neben <tedneben@gmail.com>
2020-05-20 22:17:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def greet_characters(array)
-  array.each { |greeting| puts "Hello #{array[0]}!" }
+  array.each { |greeting| puts "Hello #{array[1]}!" }
 end
 
 # Use `each` to enumerate over the provided array
